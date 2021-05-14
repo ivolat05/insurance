5441321dd59ddebb10ca2a97685560510abfce95
andrei_labadaev <53191183+ivolat05@users.noreply.github.com>
2021-05-14 14:37:25 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(function(){
         $('.menu__burger').toggleClass('burger__active'),
             $('.menu').toggleClass('menu__active'),
         $('.header__inner').toggleClass('header__inner-active'),
-            $('.main__box').toggleClass('main__box-active'),
-            $('.main__wrapper').toggleClass('main__wrapper-active')
+            $('.main__box-conteiner').toggleClass('main__box-active')
+           
     });
 });
