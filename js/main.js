826f7aa1bb404b7ebb1f21befe092e703a8672ac
andrei_labadaev <53191183+ivolat05@users.noreply.github.com>
2021-05-16 11:32:04 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,4 +6,29 @@ $(function(){
             $('.main__box-conteiner').toggleClass('main__box-active')
            
     });
+
+    // scroll table
+    const scrollStart = $('#scrollStart').offset().top,
+           scrollFinish = $('#scrollFinish').offset().top,
+            navHeiht = $('#column__height').height();
+
+    
+    
+    $(window).scroll(function () {
+        const scrolled = $(this).scrollTop();
+        
+        if (scrolled > scrollStart) {
+            // fixed head
+            $('#scrollStart').addClass('table__line-fixed');
+            $('#line__margin').css({ marginBottom: navHeiht });
+        }  else {
+            // not fixed  head
+            $('#scrollStart').removeClass('table__line-fixed');
+            $('#line__margin').css({ marginBottom: 0});
+        }
+        if (scrolled > scrollFinish) {
+            $('#scrollStart').removeClass('table__line-fixed');
+            $('#line__margin').css({ marginBottom: 0});
+        }
+    })
 });
